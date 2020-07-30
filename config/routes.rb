c8c949dda8052cc9f832215db7303c9ddb2ddce4
Dmitry Non <mail@nondv.io>
2020-07-30 12:01:38 +0100

    replace routes with "/" and "/guess"

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
-  get 'favourite_language_guess/index'
-  get 'favourite_language_guess/show'
+  root to: 'favourite_language_guess#index'
+  get '/guess', to: 'favourite_language_guess#show'
+
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
