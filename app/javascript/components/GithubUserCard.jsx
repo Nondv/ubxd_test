b4f8b38dc12e019c59a547146c85e9994955412b
Dmitry Non <mail@nondv.io>
2020-07-31 12:53:34 +0100

    implement required design in GithubUserCard

--- app/javascript/components/GithubUserCard.jsx
+++ app/javascript/components/GithubUserCard.jsx
@@ -1,11 +1,18 @@
 import React from "react"
 
+import './GithubUserCard.sass'
+
 function GithubUserCard({ username, name, bio, avatar_url }) {
+  const nameElement = <div className='github-user-card__name'>{name}</div>;
+  const usernameElement = <div className='github-user-card__username'>@{username}</div>;
+
   return (
-    <div>
-      <img src={avatar_url} />
-      <div><strong>{name}</strong> @{username}</div>
-      <p>{bio}</p>
+    <div className='github-user-card'>
+      <img className='github-user-card__avatar' alt='avatar' src={avatar_url} />
+      <div className='github-user-card__content'>
+        <div className='github-user-card__header'>{nameElement} {usernameElement}</div>
+        <div className='github-user-card__bio'>{bio}</div>
+      </div>
     </div>
   )
 }
