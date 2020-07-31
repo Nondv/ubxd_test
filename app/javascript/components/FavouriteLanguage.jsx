5865a324357767a7078542266c0809b41ee4b463
Dmitry Non <mail@nondv.io>
2020-07-31 10:24:05 +0100

    handle situation when cant guess the language

--- app/javascript/components/FavouriteLanguage.jsx
+++ app/javascript/components/FavouriteLanguage.jsx
@@ -3,6 +3,11 @@ import './FavouriteLanguage.sass'
 
 function FavouriteLanguage({ username, language }) {
   const usernameElement = <span className='favourite-language__username'>{username}</span>;
+
+  if(!language) {
+    return <div>Can't guess {usernameElement}'s favourite language :(</div>;
+  }
+
   const languageElement = <span className='favourite-language__language'>{language}</span>;
 
   return (
